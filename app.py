1ad6327c0ab849bd309c8b1161672a8ce630b482
type9 <gabespersonal@gmail.com>
2019-09-30 14:40:29 -0700

    Added destroy button

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
-from bson.objectid import ObjectId
-from flask import Flask, render_template, request, redirect, url_for
 
 client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
+
+from bson.objectid import ObjectId
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -12,11 +13,6 @@ app = Flask(__name__)
     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
 ] """
-
-@app.route('/old')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
 
 @app.route('/')
 def playlists_index():
@@ -34,11 +30,11 @@ def playlists_submit():
     playlist_id = playlists.insert_one(playlist).inserted_id
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
-@app.route('/playlists/new')
-def playlists_new():
-    """Create a new playlist."""
-    return render_template('playlists_new.html', playlist={}, title='New Playlist')
-
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
 
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
@@ -53,11 +49,22 @@ def playlists_update(playlist_id):
         {'$set': updated_playlist})
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
+
 @app.route('/playlists/<playlist_id>/edit')
 def playlists_edit(playlist_id):
     """Show the edit form for a playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """Delete one playlist."""
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
     app.run(debug=True)

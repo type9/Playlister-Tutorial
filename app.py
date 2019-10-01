d2daee255d14be3afbd151d61471595d47c9b5fe
type9 <gabespersonal@gmail.com>
2019-10-01 16:57:03 -0700

    Changed retrywrite? to false

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 
